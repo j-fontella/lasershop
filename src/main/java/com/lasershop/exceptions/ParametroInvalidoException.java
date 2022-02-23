bc77b04d9b193a601b4c72aed0cffc2736dfddd8
Fontella <joaofontella11@gmail.com>
2022-02-23 05:08:54 -0300

    Criadas exceções para parametros e pedidos inválidos.

---
+++ src/main/java/com/lasershop/exceptions/ParametroInvalidoException.java
@@ -0,0 +1,7 @@
+package com.lasershop.exceptions;
+
+public class ParametroInvalidoException extends RuntimeException{
+    public ParametroInvalidoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/lasershop/exceptions/PedidoInvalidoException.java
@@ -0,0 +1,7 @@
+package com.lasershop.exceptions;
+
+public class PedidoInvalidoException extends RuntimeException{
+    public PedidoInvalidoException(String message) {
+        super(message);
+    }
+}
