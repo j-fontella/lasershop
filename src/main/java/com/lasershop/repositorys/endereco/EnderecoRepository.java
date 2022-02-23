14507d2d0e3379f342fad8deb32c19cca6917712
Fontella <joaofontella11@gmail.com>
2022-02-23 05:07:38 -0300

    Adicionado metodo para consulta por id

--- src/main/java/com/lasershop/repositorys/endereco/EnderecoRepository.java
+++ src/main/java/com/lasershop/repositorys/endereco/EnderecoRepository.java
@@ -4,6 +4,10 @@ import com.lasershop.models.endereco.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+    Optional<Endereco> getEnderecoById(Long id);
 }
