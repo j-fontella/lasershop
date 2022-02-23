74dd84ad8278ce29ed8dbf16c4e9b42c4908dcee
Fontella <joaofontella11@gmail.com>
2022-02-23 05:23:11 -0300

    Adicionada validação para pedidos ocorrerem apenas dia de semana

--- src/main/java/com/lasershop/domains/Erros.java
+++ src/main/java/com/lasershop/domains/Erros.java
@@ -10,6 +10,7 @@ public enum Erros {
     PRODUTO_NAO_ENCONTRADO("Este prodúto não foi encontrado no sistema."),
     PRODUTO_JA_CADASTRADO("Este prodúto já está cadastrado no sistema"),
     PEDIDO_HORARIO_NAO_PERMITIDO("Os pedidos só podem ser feitos entre 8:00 e 18:00"),
+    PEDIDO_FORA_DIA_UTIL("Pedidos não podem ser feitos aos finais de semanas"),
     NUMERO_PARCELAS_SUPERIOR_LIMITE("O numero de parcelas solicitada para compra é superior ao do cliente"),
     VALOR_PEDIDO_SUPERIOR_LIMITE_CREDITO("O valor da compra é superior ao limite de crédito do cliente"),
     MENSAGEM_ERRO_DEFAULT("Um erro ocorreu nesta operação, recarregue a página, tente novamente e caso persista contate o suporte.");
