45a5064e3ad8e16aea29b963d4d982e34d9dd65d
Fontella <joaofontella11@gmail.com>
2022-02-23 05:07:10 -0300

    Inserido metodo para deletar usuário e ajustado os de registro/edição

--- src/main/java/com/lasershop/services/login/LoginService.java
+++ src/main/java/com/lasershop/services/login/LoginService.java
@@ -3,6 +3,7 @@ package com.lasershop.services.login;
 import com.lasershop.domains.Erros;
 import com.lasershop.dtos.request.login.UsuarioRequestDTO;
 import com.lasershop.dtos.response.login.UsuarioResponseDTO;
+import com.lasershop.exceptions.ParametroInvalidoException;
 import com.lasershop.models.endereco.Endereco;
 import com.lasershop.models.login.Usuario;
 import com.lasershop.repositorys.endereco.EnderecoRepository;
@@ -33,11 +34,15 @@ public class LoginService {
         if(usuario.isPresent()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Utils.gerarErro(Erros.USUARIO_JA_CADASTRADO.getDescricao()));
         }
-        salvarUsuario(usuarioRequestDTO);
+        try {
+            salvarUsuario(usuarioRequestDTO);
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Utils.gerarErro(ex.getMessage()));
+        }
         return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<?> editarUsuario(UsuarioRequestDTO usuarioRequestDTO) throws NoSuchAlgorithmException {
+    public ResponseEntity<?> editarUsuario(UsuarioRequestDTO usuarioRequestDTO) {
         if(Objects.isNull(usuarioRequestDTO.getId())){
             return ResponseEntity.badRequest().body(Utils.gerarErro(Erros.MENSAGEM_ERRO_DEFAULT.getDescricao()));
         }
@@ -45,7 +50,24 @@ public class LoginService {
         if(usuario.isEmpty()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Utils.gerarErro(Erros.USUARIO_NAO_ENCONTRADO.getDescricao()));
         }
-        salvarUsuario(usuarioRequestDTO);
+        try {
+            salvarUsuario(usuarioRequestDTO);
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Utils.gerarErro(ex.getMessage()));
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<?> deletarUsuario(Long id) {
+        if(Objects.isNull(id)){
+            return ResponseEntity.badRequest().body(Utils.gerarErro(Erros.MENSAGEM_ERRO_DEFAULT.getDescricao()));
+        }
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if(usuario.isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Utils.gerarErro(Erros.USUARIO_NAO_ENCONTRADO.getDescricao()));
+        }
+        usuarioRepository.delete(usuario.get());
+        enderecoRepository.delete(usuario.get().getEndereco());
         return ResponseEntity.ok().build();
     }
 
@@ -68,9 +90,16 @@ public class LoginService {
         usuarioRequestDTO.setSenha(senhaCriptografada);
 
         Endereco enderecoACadastrar;
-        Endereco enderecoUsuario = usuarioRequestDTO.getEndereco();
+        Endereco enderecoUsuario = Utils.converterEnderecoRequestToBase(usuarioRequestDTO.getEndereco());
         Long idEnderecoUsuario = enderecoUsuario.getId();
 
+        Long idEndereco = usuarioRequestDTO.getEndereco().getId();
+        if(Objects.nonNull(idEndereco)){
+            Optional<Endereco> endereco = enderecoRepository.getEnderecoById(idEndereco);
+            if(endereco.isEmpty()){
+                throw new ParametroInvalidoException(Erros.MENSAGEM_ERRO_DEFAULT.getDescricao());
+            }
+        }
 
         if(idEnderecoUsuario == null){
             enderecoACadastrar = enderecoRepository.save(enderecoUsuario);
@@ -83,6 +112,7 @@ public class LoginService {
         Usuario usuarioCadastrado = Utils.converterUsuarioRequest(usuarioRequestDTO, enderecoACadastrar);
         usuarioRepository.save(usuarioCadastrado);
     }
+
 
 
 }
